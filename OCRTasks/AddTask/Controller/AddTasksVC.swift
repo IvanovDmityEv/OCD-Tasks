26f87d5c775a2224722456cf173974900e1819b7
Dmitry <idekas92@gmail.com>
2022-12-23 17:19:25 +0300

    add datePicker

--- OCRTasks/AddTask/Controller/AddTasksVC.swift
+++ OCRTasks/AddTask/Controller/AddTasksVC.swift
@@ -12,6 +12,13 @@ class AddTasksVC: UIViewController {
 
     var arrayTasks: [String] = []
     var tap: [String] = []
+    var dateTasks: String? {
+        didSet {
+            guard dateTasks != nil else  { return }
+            buttoAddDate.titleLabel?.text = dateTasks
+        }
+    }
+    var timeTasks: String = " Time"
     private var obsserverCell: Bool = true
 //    var cell: CellTask?
     
@@ -20,15 +27,39 @@ class AddTasksVC: UIViewController {
     var tasksList = Array<Tasks>()
     
     @IBOutlet weak var nameTasksList: UITextField!
-    @IBOutlet weak var switchDate: UISwitch!
-    @IBOutlet weak var datePicker: UIDatePicker!
-    @IBOutlet weak var switchTime: UISwitch!
-    @IBOutlet weak var timePicker: UIDatePicker!
+    
+    @IBOutlet weak var stackViewButton: UIStackView! {
+        didSet {
+            stackViewButton.layer.cornerRadius = 7
+        }
+    }
+    
+    
+    @IBOutlet weak var buttoAddDate: UIButton! {
+        didSet {
+            buttoAddDate.layer.cornerRadius = 7
+//            buttoAddDate.titleLabel?.text = " Date"
+//            guard dateTasks != nil else { return }
+//            buttoAddDate.titleLabel?.text = dateTasks
+        }
+    }
+    
+    
+    @IBOutlet weak var buttonAddTime: UIButton! {
+        didSet {
+            buttonAddTime.layer.cornerRadius = 7
+            buttonAddTime.titleLabel?.text = timeTasks
+        }
+    }
+    
+    
+    
     @IBOutlet weak var buttonAddNewTask: UIButton! {
         didSet {
             buttonAddNewTask.layer.cornerRadius = 7
         }
     }
+
     @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
@@ -52,6 +83,90 @@ class AddTasksVC: UIViewController {
         view.endEditing(true)
     }
     
+    @IBAction func actionAddDate(_ sender: UIButton) {
+
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let datePicker = UIDatePicker()
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .date
+        
+//        let dateFormater = DateFormatter()
+//        if dateTasks != nil {
+//            datePicker.date = dateFormater.date(from: dateTasks)
+//        }
+        
+        alertController.view.addSubview(datePicker)
+        
+
+        
+        
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        
+        alertController.view.heightAnchor.constraint(equalToConstant: 350).isActive = true
+
+        datePicker.centerXAnchor.constraint(equalTo: alertController.view.centerXAnchor).isActive = true
+        datePicker.topAnchor.constraint(equalTo: alertController.view.topAnchor, constant: 30).isActive = true
+
+        let save = UIAlertAction(title: "Save", style: .cancel) { [weak self] (action) in
+            
+            let dateFormater = DateFormatter()
+            dateFormater.dateStyle = .short
+            self?.dateTasks = dateFormater.string(from: datePicker.date)
+
+            print((self?.dateTasks)!)
+            
+            
+        }
+        
+        let clear = UIAlertAction(title: "Clear", style: .destructive) { [weak self] (action) in
+            self?.dateTasks = nil
+        }
+        alertController.addAction(clear)
+        alertController.addAction(save)
+        
+        present(alertController, animated: true)
+        
+    }
+    
+    @IBAction func actionAddTime(_ sender: UIButton) {
+        
+        let alertController = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        
+        let datePicker = UIDatePicker()
+        datePicker.preferredDatePickerStyle = .wheels
+        datePicker.datePickerMode = .time
+        
+        alertController.view.addSubview(datePicker)
+        
+        datePicker.translatesAutoresizingMaskIntoConstraints = false
+        
+        alertController.view.heightAnchor.constraint(equalToConstant: 350).isActive = true
+
+        datePicker.centerXAnchor.constraint(equalTo: alertController.view.centerXAnchor).isActive = true
+        datePicker.topAnchor.constraint(equalTo: alertController.view.topAnchor, constant: 30).isActive = true
+
+        let save = UIAlertAction(title: "Save", style: .cancel) { [weak self] (action) in
+            
+            let dateFormater = DateFormatter()
+            dateFormater.timeStyle = .short
+            self?.timeTasks = dateFormater.string(from: datePicker.date)
+            
+            print((self?.timeTasks)!)
+        }
+        
+        let clear = UIAlertAction(title: "Clear", style: .destructive) { [weak self] (action) in
+            self?.timeTasks = " Time"
+        }
+        alertController.addAction(clear)
+        alertController.addAction(save)
+        
+        present(alertController, animated: true)
+        
+    }
+    
+    
+
 
     @IBAction func actionAddNewTask(_ sender: UIButton) {
 //        countTap += 1
@@ -87,31 +202,6 @@ extension AddTasksVC: UITableViewDataSource, UITableViewDelegate {
         return cell
     }
     
-//    func tableView(_ tableView: UITableView, willSelectRowAt indexPath: IndexPath) -> IndexPath? {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "CellTask", for: indexPath) as! CellTask
-//        if cell.newTaskTextField.text != "" {
-//            obsserverCell = false
-//        }
-//        return indexPath
-//    }
-    
-//    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
-//        let cell = tableView.dequeueReusableCell(withIdentifier: "CellTask", for: indexPath) as! CellTask
-//
-//                arrayTasks.append(textTask.text!)
-//                tableView.reloadData()
-//                print("кол-во элементов: \(arrayTasks.count)")
-        
-//        if obsserverCell == true {
-//            arrayTasks.append(textTask.text!)
-//            tableView.reloadData()
-//            printContent("кол-во элементов: \(arrayTasks.count)")
-//        } else {
-//            arrayTasks.insert(textTask.text!, at: 0)
-//            tableView.reloadData()
-//
-        
-//    }
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let cell = tableView.dequeueReusableCell(withIdentifier: "CellTask", for: indexPath) as! CellTask
         
